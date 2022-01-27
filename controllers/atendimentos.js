7e6a87870e42e64c195f3f79ac9f2fe55be1b64e
valdirapn <valdiraires.pn@gmail.com>
2022-01-27 02:07:22 -0300

    saving data in db

--- controllers/atendimentos.js
+++ controllers/atendimentos.js
@@ -1,10 +1,14 @@
+const Atendimentos = require('../models/atendimentos')
+
 module.exports = (app) => {
     app.get('/atendimentos', (req, res) => {
         res.send('Atendimentos GET')
     })
 
     app.post('/atendimentos', (req, res) => {
-        console.log(req.body)
+        const atendimento = req.body
+        
+        Atendimentos.adiciona(atendimento)
         res.send('Atendimentos POST')
     })
 }
--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 const customExpress = require('./config/customExpress')
 const conexao = require('./infraestrutura/conexao')
+const Tabelas = require('./infraestrutura/tabelas')
 
 conexao.connect((erro) => {
     if (erro) {
         console.log(erro)
     } else {
-        console.log('Conectado no BD com sucesso')
+        console.log('Conectado com sucessono BD')
 
+        Tabelas.init(conexao)
         const app = customExpress()
 
         app.listen(3000, () => console.log('Server is running on port 3000'))
