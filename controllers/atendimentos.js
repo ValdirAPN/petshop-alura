46f9687101945a58ec67bf833842e8b9ede9dee3
valdirapn <valdiraires.pn@gmail.com>
2022-01-27 01:34:06 -0300

    req body parser

--- controllers/atendimentos.js
+++ controllers/atendimentos.js
@@ -1,5 +1,10 @@
 module.exports = (app) => {
     app.get('/atendimentos', (req, res) => {
-        res.send('Servidor rodando, tudo ok')
+        res.send('Atendimentos GET')
+    })
+
+    app.post('/atendimentos', (req, res) => {
+        console.log(req.body)
+        res.send('Atendimentos POST')
     })
 }
